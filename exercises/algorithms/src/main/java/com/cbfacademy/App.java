012806caeb019fa92c9e90e9e5a454e18bfec334
Siobhan Baker <sohbaker@gmail.com>
2023-09-04 20:29:19 +0100

    add solution for binary search algorithm exercise

--- exercises/algorithms/src/main/java/com/cbfacademy/App.java
+++ exercises/algorithms/src/main/java/com/cbfacademy/App.java
@@ -1,7 +1,13 @@
 package com.cbfacademy;
 
+import com.cbfacademy.search.SearchAlgoExercise;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SearchAlgoExercise searchAlgoExercise = new SearchAlgoExercise();
+        int[] sortedArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int target = 12;
+
+        System.out.println(searchAlgoExercise.binarySearch(sortedArray, target));
     }
 }
---
+++ exercises/algorithms/src/main/java/com/cbfacademy/search/Search.java
@@ -0,0 +1,7 @@
+package com.cbfacademy.search;
+
+public interface Search {
+    int binarySearch(int[] sortedArray, int target);
+
+    String getName();
+}
