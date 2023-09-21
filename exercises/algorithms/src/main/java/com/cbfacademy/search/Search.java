215c9f226050e0e93059169f0c55a08199ca4712
Siobhan Baker <sohbaker@gmail.com>
2023-09-21 22:59:29 +0100

    add tests for binary search algorithm exercise

---
+++ exercises/algorithms/src/main/java/com/cbfacademy/search/Search.java
@@ -0,0 +1,17 @@
+package com.cbfacademy.search;
+
+public interface Search {
+    /**
+     * Searches an array.
+     *
+     * @param sortedArray The array to be searched (the array should already be s
+     *                    rted)
+     * @param target      The value to be searched for in the array
+     */
+    int search(int[] sortedArray, int target);
+
+    /**
+     * Returns the exercise name
+     */
+    String getName();
+}
---
+++ exercises/algorithms/src/test/java/com/cbfacademy/search/BinarySearchTest.java
@@ -0,0 +1,34 @@
+package com.cbfacademy.search;
+
+public class BinarySearch implements Search {
+    /**
+     * Searches an array using the Binary Search algorithm.
+     *
+     * @param sortedArray The array to be searched (the array should be sorted to
+     *                    prevent unpredicatable results)
+     * @param target      The value to be searched for in the array
+     */
+    public int search(int[] sortedArray, int target) {
+        int low = 0;
+        int high = sortedArray.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (target == sortedArray[mid])
+                return mid;
+            else if (target > sortedArray[mid])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+
+    }
+
+    /**
+     * Returns the exercise name
+     */
+    public String getName() {
+        return "Binary Search Algorithm Exercise";
+    }
+}
