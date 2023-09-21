c45561b4ffd95e62ab54296fdad5e2f8d6506f80
Siobhan Baker <sohbaker@gmail.com>
2023-09-21 17:26:38 +0100

    add solution for io homework task

--- homework/input-output/src/main/java/com/cbfacademy/App.java
+++ homework/input-output/src/main/java/com/cbfacademy/App.java
@@ -1,7 +1,25 @@
 package com.cbfacademy;
 
+import java.util.HashMap;
+import java.util.Properties;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String existingFilePath = "homework/input-output/src/test/resources";
+
+        HashMap<String, String> propMap = new HashMap<String, String>() {
+            {
+                put("db.url", "localhost");
+                put("db.port", "5353");
+                put("db.user", "cbfacademy");
+                put("db.password", "password");
+            }
+        };
+        String newFilePath = "homework/input-output/src/test/resources/output_test_file.properties";
+
+        PropertiesFileHandler.loadProperties(existingFilePath);
+        Properties createdProperties = PropertiesFileHandler.createFile(propMap, newFilePath);
+
+        System.out.println("File created: " + createdProperties.toString());
     }
 }
