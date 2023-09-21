64d308189b261ac6623fcc70bddf9be806e05997
Siobhan Baker <sohbaker@gmail.com>
2023-09-21 16:21:40 +0100

    move App file to search folder

--- exercises/algorithms/src/main/java/com/cbfacademy/search/App.java
+++ exercises/algorithms/src/main/java/com/cbfacademy/search/App.java
@@ -1,12 +1,10 @@
-package com.cbfacademy;
-
-import com.cbfacademy.search.BinarySearch;
+package com.cbfacademy.search;
 
 public class App {
     public static void main(String[] args) {
         BinarySearch binarySearch = new BinarySearch();
         int[] sortedArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        int target = 12;
+        int target = 6;
 
         System.out.println(binarySearch.search(sortedArray, target));
     }
