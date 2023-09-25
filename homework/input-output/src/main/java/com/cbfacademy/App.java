e7f867582e1b622d1c786fed429bd513651b9d25
Siobhan Baker <sohbaker@gmail.com>
2023-09-25 14:29:34 +0100

    update filepaths

--- homework/input-output/src/main/java/com/cbfacademy/App.java
+++ homework/input-output/src/main/java/com/cbfacademy/App.java
@@ -5,7 +5,7 @@ import java.util.Properties;
 
 public class App {
     public static void main(String[] args) {
-        String existingFilePath = "homework/input-output/src/test/resources/test_file.properties";
+        String existingFilePath = "homework/input-output/src/main/resources/example.properties";
 
         HashMap<String, String> propMap = new HashMap<String, String>() {
             {
@@ -15,7 +15,7 @@ public class App {
                 put("datasource.password", "password");
             }
         };
-        String newFilePath = "homework/input-output/src/test/resources/output_test_file.properties";
+        String newFilePath = "homework/input-output/src/main/resources/configuration.properties";
 
         PropertiesFileHandler.loadProperties(existingFilePath);
         Properties createdProperties = PropertiesFileHandler.createFile(propMap, newFilePath);
