fc2c0b9abbb553a84a39c29a06d2ba60c43d871b
Genyus <genyus@gmail.com>
2024-07-03 22:57:41 +0100

    feat: implements server connection exercise

--- exercises/client-server-architecture/src/main/java/com/cbfacademy/App.java
+++ exercises/client-server-architecture/src/main/java/com/cbfacademy/App.java
@@ -1,7 +1,34 @@
 package com.cbfacademy;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            URL url = new URI("https://codingblackfemales.com").toURL();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            // Setting the request method to GET
+            connection.setRequestMethod("GET");
+
+            // Using try-with-resources to ensure resources are closed
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                String inputLine;
+
+                while ((inputLine = in.readLine()) != null) {
+                    System.out.println(inputLine);
+                }
+            }
+
+            // Closing the connection
+            connection.disconnect();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
